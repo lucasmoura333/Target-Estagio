2f242f5e95b0710c7328979b7cbfed35ed6c8fb8
Lucas Moura <lborges.moura@gmail.com>
2024-09-03 16:55:50 -0400

    [001]Commit pronto rs
    Requisito 3,
    
    R = 77.

---
+++ Respostas/r3/Terceiro.java
@@ -0,0 +1,19 @@
+package Respostas.r3;
+
+//------------------------------ soma = 77
+
+public class Terceiro {
+
+    public static void main(String[] args) {
+        int indice = 12, soma = 0, k = 1;
+
+        while( k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println("O total da variável SOMA é: " + soma);
+        
+    }
+    
+}
